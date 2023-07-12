77e90de710ee0073c4833f79504b15c0009fea05
sdulal1991 <139015857+sdulal1991@users.noreply.github.com>
2023-07-12 14:08:18 +0100

    Add files via upload

---
+++ assets/vendor/php-email-form/php-email-form.php
@@ -0,0 +1,27 @@
+
+                        <?php 
+                            $Msg = "";
+                            if(isset($_GET['error']))
+                            {
+                                $Msg = " Please Fill in the Blanks ";
+                                echo '<div class="alert alert-danger">'.$Msg.'</div>';
+                            }
+
+                            if(isset($_GET['success']))
+                            {
+                                $Msg = " Your Message Has Been Sent ";
+                                echo '<div class="alert alert-success">'.$Msg.'</div>';
+                            }
+                        
+                        ?>
+                    </div>
+                    <div class="card-body">
+                        <form action="process.php" method="post">
+                            <input type="text" name="UName" placeholder="User Name" class="form-control mb-2">
+                            <input type="email" name="Email" placeholder="Email" class="form-control mb-2">
+                            <input type="text" name="Subject" placeholder="Subject" class="form-control mb-2">
+                            <textarea name="msg" class="form-control mb-2" placeholder="Write The Message"></textarea>
+                            <button class="btn btn-success" name="btn-send"> Send </button>
+                        </form>
+                    </div>
+                </div>
